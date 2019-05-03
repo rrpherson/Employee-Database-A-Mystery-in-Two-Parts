1ed9bed18d71f0829f6a7a863ef4bf86d9963312
Richard Pherson <rrpherson@gmail.com>
2019-05-03 16:54:56 -0400

    updating hw

--- HW7_Pewlett_Hackard.sql
+++ HW7_Pewlett_Hackard.sql
@@ -5,7 +5,7 @@ SELECT employees.emp_no,
 		employees.gender, 
 		salaries.salary
 FROM employees
-JOIN salaries ON employees.emp_no = salaries.emp_no;
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
 -- 2. List employees who were hired in 1986.
 SELECT first_name, last_name, hire_date FROM employees 
@@ -20,8 +20,8 @@ SELECT departments.dept_no,
 		dept_manager.from_date, 
 		dept_manager.to_date
 FROM departments
-JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
-JOIN employees ON dept_manager.emp_no = employees.emp_no;
+INNER JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+INNER JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, 
@@ -29,8 +29,8 @@ SELECT dept_emp.emp_no,
 		employees.first_name, 
 		departments.dept_name
 FROM dept_emp
-JOIN employees ON dept_emp.emp_no = employees.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no;
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no;
 
 -- 5. List all employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name FROM employees
@@ -42,8 +42,8 @@ SELECT dept_emp.emp_no,
 		employees.first_name, 
 		departments.dept_name
 FROM dept_emp
-JOIN employees ON dept_emp.emp_no = employees.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
@@ -52,8 +52,8 @@ SELECT dept_emp.emp_no,
 		employees.first_name, 
 		departments.dept_name
 FROM dept_emp
-JOIN employees ON dept_emp.emp_no = employees.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
